33f4ded408eb7b9bfa995f5ddad82518cd2eeee8
ZefanyaTarigan-2311102028 <zefanyabrana@gmail.com>
2024-11-10 21:24:53 +0700

    Modul5

---
+++ 2/2.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	var n int
+	fmt.Print("Masukkan angka: ")
+	fmt.Scanln(&n)
+	printStar(n)
+}
+
+func printStar(n int) {
+	if n == 0 {
+		return
+	}
+	printStar(n - 1)
+	for i := 0; i < n; i++ {
+		fmt.Print("*")
+	}
+	fmt.Println()
+}
